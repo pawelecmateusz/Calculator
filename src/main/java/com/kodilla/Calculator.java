0f5adb9593e9ae7da1cb21525b36e79b21ce76a9
Mateusz Pawelec <mateuszpawelecprv@gmail.com>
2023-01-08 12:25:47 +0100

    Calculator

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,13 +1,13 @@
 package com.kodilla;
 
 public class Calculator {
-    public void adding() {
+    public void addNumbers() {
         int one = 1;
         int two = 2;
         int addResult = one + two;
         System.out.println("1 + 2 = " + addResult);
     }
-    public void substraction() {
+    public void substractNumbers() {
         int three = 3;
         int four = 4;
         int subResult = four - three;
@@ -15,7 +15,7 @@ public class Calculator {
     }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.adding();
-        calculator.substraction();
+        calculator.addNumbers();
+        calculator.substractNumbers();
     }
 }
